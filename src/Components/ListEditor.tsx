ce566b4dab0c610c529265b9f6bcfdac3a0ad6c0
Sondre Widmark <sondre.widmark@gmail.com>
2022-01-20 21:21:57 +0100

    Make the list sortable with drag and drop

--- src/Components/ListEditor.tsx
+++ src/Components/ListEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { SearchItem } from '../Models/BackendModels';
 import { RecommendationList } from '../State/Atoms';
@@ -9,9 +9,17 @@ import {
   Heading,
   Image,
   Input,
+  List,
+  ListItem,
   Text,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 
 const ListEditor: React.FC = () => {
   const [listName, setListName] = useState<string>('');
@@ -20,88 +28,132 @@ const ListEditor: React.FC = () => {
   const [recommendationList, setRecommendationList] =
     useRecoilState<SearchItem[]>(RecommendationList);
 
+  const [editedList, setEditedList] =
+    useState<SearchItem[]>(recommendationList);
+
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    if (!!destination && !!source) {
+      const newSort = [...editedList];
+      const element = newSort[result.source.index];
+      newSort.splice(source.index, 1);
+      newSort.splice(destination.index, 0, element);
+      setEditedList(newSort);
+    }
+  };
+
   return (
-    <Container>
-      {recommendationList.length === 0 && (
-        <Heading as="h2" size="lg" pt={3} mb={6}>
-          You have no movies or shows to edit, go to the{' '}
-          <Link style={{ color: 'blue' }} to="/">
-            main page
-          </Link>
-        </Heading>
-      )}
-      {recommendationList.length > 0 && (
-        <>
-          <Heading pt={3} mb={6} as="h1" size="md">
-            List editor
+    <DragDropContext onDragEnd={(dropResult) => handleOnDragEnd(dropResult)}>
+      <Container>
+        {editedList.length === 0 && (
+          <Heading as="h2" size="lg" pt={3} mb={6}>
+            You have no movies or shows to edit, go to the{' '}
+            <Link style={{ color: 'blue' }} to="/">
+              main page
+            </Link>
           </Heading>
-          <Input
-            onChange={(event) => setListName(event.target.value)}
-            pl={5}
-            value={listName}
-            fontSize={'x-large'}
-            variant="flushed"
-            size="lg"
-            placeholder="Name of list"
-            mb={5}
-          />
+        )}
+        {editedList.length > 0 && (
+          <>
+            <Heading pt={3} mb={6} as="h1" size="md">
+              List editor
+            </Heading>
+            <Input
+              onChange={(event) => setListName(event.target.value)}
+              pl={5}
+              value={listName}
+              fontSize={'x-large'}
+              variant="flushed"
+              size="lg"
+              placeholder="Name of list"
+              mb={5}
+            />
 
-          <Input
-            onChange={(event) => setListDescription(event.target.value)}
-            pl={5}
-            value={listDescription}
-            fontSize={'large'}
-            variant="flushed"
-            size="lg"
-            placeholder="Description (optional)"
-            mb={5}
-          />
-
-          {recommendationList.map((mediaItem, index) => (
-            <Box
-              border="1px"
-              borderColor="grey"
-              display="flex"
-              alignItems="center"
-              justifyContent="flex-start"
-              _hover={{ backgroundColor: 'black', cursor: 'row-resize' }}
-              p={1}
-              maxH={80}
-              key={mediaItem.id}
-            >
-              <Image
-                objectFit={'fill'}
-                maxW={60}
-                maxH={100}
-                src={mediaItem.posterUrl}
-                alt={`Poster of movie ${mediaItem.title}`}
-                // fallbackSrc={fallbackPoster}
-              />
-              <Text fontSize="lg" as="h6" pl={3}>
-                {mediaItem.title}
-                <Text display="inline-flex" color="lightgray" pl={2}>
-                  ({mediaItem.releaseDate.substring(0, 4)})
-                </Text>
-              </Text>
-              <Badge
-                alignSelf="flex-start"
-                marginLeft="auto"
-                borderRadius="full"
-                px="2"
-                py={1}
-                color="white"
-                backgroundColor={
-                  mediaItem.mediaType.toString() === 'tv' ? 'green' : 'red'
-                }
-                textShadow="0 0 3px black"
-              >
-                {mediaItem.mediaType.toString().toUpperCase()}
-              </Badge>
-            </Box>
-          ))}
-        </>
-      )}
-    </Container>
+            <Input
+              onChange={(event) => setListDescription(event.target.value)}
+              pl={5}
+              value={listDescription}
+              fontSize={'large'}
+              variant="flushed"
+              size="lg"
+              placeholder="Description (optional)"
+              mb={5}
+            />
+            <Droppable droppableId="mediaItem">
+              {(provided) => (
+                <List {...provided.droppableProps} ref={provided.innerRef}>
+                  {editedList.map((mediaItem, index) => (
+                    <Draggable
+                      draggableId={mediaItem.id.toString()}
+                      index={index}
+                      key={mediaItem.id}
+                    >
+                      {(providedItem) => (
+                        <ListItem
+                          ref={providedItem.innerRef}
+                          {...providedItem.draggableProps}
+                          {...providedItem.dragHandleProps}
+                        >
+                          <Box
+                            border="1px"
+                            borderColor="grey"
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="flex-start"
+                            _hover={{
+                              backgroundColor: 'black',
+                              cursor: 'row-resize',
+                            }}
+                            p={1}
+                            maxH={80}
+                          >
+                            <Image
+                              objectFit={'fill'}
+                              maxW={60}
+                              maxH={100}
+                              src={mediaItem.posterUrl}
+                              alt={`Poster of movie ${mediaItem.title}`}
+                              // fallbackSrc={fallbackPoster}
+                            />
+                            <Text fontSize="lg" as="h6" pl={3}>
+                              {mediaItem.title}
+                              <Text
+                                display="inline-flex"
+                                color="lightgray"
+                                pl={2}
+                              >
+                                ({mediaItem.releaseDate.substring(0, 4)})
+                              </Text>
+                            </Text>
+                            <Badge
+                              alignSelf="flex-start"
+                              marginLeft="auto"
+                              borderRadius="full"
+                              px="2"
+                              py={1}
+                              color="white"
+                              backgroundColor={
+                                mediaItem.mediaType.toString() === 'tv'
+                                  ? 'green'
+                                  : 'red'
+                              }
+                              textShadow="0 0 3px black"
+                            >
+                              {mediaItem.mediaType.toString().toUpperCase()}
+                            </Badge>
+                          </Box>
+                        </ListItem>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </List>
+              )}
+            </Droppable>
+          </>
+        )}
+      </Container>
+    </DragDropContext>
   );
 };
 
