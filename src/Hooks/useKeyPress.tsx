ef5c355d4b8e98054a455ec7bd145b04fdf226f8
Sondre Widmark <sondre.widmark@gmail.com>
2022-04-03 18:15:23 +0200

    Create hook for listening on keypress events

---
+++ src/Hooks/useKeyPress.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, RefObject } from "react";
+
+const useKeyPress = (targetKey: string, ref: RefObject<HTMLDivElement>) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  useEffect(() => {
+    const downHandler = ({ key }: { key: string }) => {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    };
+
+    const upHandler = ({ key }: { key: string }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, [ref]);
+
+  return keyPressed;
+};
+
+export default useKeyPress;
