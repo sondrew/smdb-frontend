615f6bacadd2a9f16cb7dcfce8127b3d5fed16f8
Sondre <sondre.widmark@gmail.com>
2022-08-26 16:10:05 +0200

    Create callable function to search for movies and tv shows

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,84 @@
 import * as functions from "firebase-functions";
-// https://firebase.google.com/docs/functions/typescript
+import axios, {AxiosResponse} from "axios";
+import {TMDbMultiSearchDto} from "./models";
+import {MediaType, SearchItem} from "../../shared/models";
 
-exports.bigben = functions.region('europe-west1').https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1
-  res.status(200).send(`<!doctype html>
-    <head>
-      <title>Time</title>
-    </head>
-    <body>
-      ${'BONG '.repeat(hours)}
-    </body>
-  </html>`);
-});
+//const IMDB_BASE_URL = "https://www.imdb.com/title/"
+const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
 
 
-exports.addMessage = functions.region('europe-west1').https.onCall((data, context) => {
-  console.log('addMessage callable');
-  console.log('data', data);
-  console.log('context', context);
+const baseUrl = 'https://api.themoviedb.org/3'
 
-  return {
-    response: "testing 12"
-  }
 
-  /* Enable app check in hosting and then use here:
-  if (context.app == undefined) {
-    throw new functions.https.HttpsError(
-      'failed-precondition',
-      'The function must be called from an App Check verified app.')
-  }
-   */
-});
+exports.searchMulti = functions.region('europe-west1').https.onCall((data, context) => {
+  console.log('searchQuery', data);
+
+  const requestUrl = `${baseUrl}/search/multi?query=${data}&${apiKey}`
+
+  return axios.get(requestUrl).then((res: AxiosResponse<TMDbMultiSearchDto>) => {
+    console.log('then');
+    console.log('then');
+    const searchResult = mapAndFilterSearchResults(res.data)
+
+    return {
+      status: 'ok',
+      result: searchResult
+    }
+    //return res.data.results
+  }).catch((err) => {
+      console.log('err');
+      console.log(err);
+      return {
+        status: 'error',
+        error: err
+      }
+  })
+
+  //console.log('request');
+  //console.log(request);
+})
+
+const mapAndFilterSearchResults = (response: TMDbMultiSearchDto): SearchItem[] => {
+  if (response.total_results === 0) return []
+
+  return response.results
+    .filter((media) => media.media_type === MediaType.TV || media.media_type === MediaType.MOVIE)
+    .map((media) => {
+      if (media.media_type === MediaType.TV) {
+        console.log(media.name);
+        return {
+          id: media.id,
+          title: media.name,
+          mediaType: MediaType.TV,
+          voteAverage: media.vote_average,
+          voteCount: media.vote_count,
+          popularity: media.popularity,
+          overview: media.overview,
+          posterUrl: getPosterUrl(media.poster_path),
+          releaseDate: media.first_air_date,
+          originalTitle: media.original_name
+        } as SearchItem
+      } else {
+        console.log(media.title);
+        return {
+          id: media.id,
+          title: media.title,
+          mediaType: MediaType.TV,
+          voteAverage: media.vote_average,
+          voteCount: media.vote_count,
+          popularity: media.popularity,
+          overview: media.overview,
+          posterUrl: getPosterUrl(media.poster_path),
+          releaseDate: media.release_date,
+          originalTitle: media.original_title
+        } as SearchItem
+      }
+    }).sort((a, b) => b.popularity - a.popularity)
+
+}
+
+const getPosterUrl = (path: string | null): string => {
+  if (path !== null) return `${POSTER_BASE_URL}${path}`
+  else return "";
+}
+
