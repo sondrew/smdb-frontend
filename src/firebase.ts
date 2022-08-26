8b60c4bb2bafa4f38b54bad943c421c19218e0e3
Sondre <sondre.widmark@gmail.com>
2022-08-26 17:10:05 +0200

    Include firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,19 @@
 
-import firebaseConfig from "./firebaseConfig";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
+import { connectFunctionsEmulator } from "firebase/functions";
 //import { getStorage, ref } from "firebase/storage";
 //import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-import { connectFunctionsEmulator } from "firebase/functions";
+const firebaseConfig = {
+  apiKey: "AIzaSyBNukhn5Ztbe2uFhvh3VYAdTiX73Bts1iU",
+  authDomain: "recommender3000.firebaseapp.com",
+  projectId: "recommender3000",
+  storageBucket: "recommender3000.appspot.com",
+  messagingSenderId: "555878506033",
+  appId: "1:555878506033:web:65bc1ab79bfbcdf418bf10"
+};
 
 const app = initializeApp(firebaseConfig);
 
