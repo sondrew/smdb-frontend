c429b6b6dd5b6676b4755a61362e936c3f7af1bf
Sondre <sondre.widmark@gmail.com>
2022-09-03 13:46:08 +0200

    Run prettier on functions

--- functions/src/service.ts
+++ functions/src/service.ts
@@ -5,38 +5,38 @@ import {
   MovieResultDto,
   ResponseStatus,
   TMDbMultiSearchDto,
-  TVResultDto
-} from "./backendModels";
-import {MediaType, SearchItem} from "../../shared/models";
-import {searchMulti} from "./tmdbGateway";
+  TVResultDto,
+} from './backendModels';
+import { MediaType, SearchItem } from '../../shared/models';
+import { searchMulti } from './tmdbGateway';
 
 //const IMDB_BASE_URL = "https://www.imdb.com/title/"
-const FALLBACK_POST_URL = 'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg';
-const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
+const FALLBACK_POST_URL =
+  'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg';
+const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-
-export const searchMovieOrTV = async (query: string, apiKey: string): Promise<SearchItem[]>  => {
-  const searchResult: ApiResponse<TMDbMultiSearchDto> = await searchMulti(query, apiKey)
+export const searchMovieOrTV = async (query: string, apiKey: string): Promise<SearchItem[]> => {
+  const searchResult: ApiResponse<TMDbMultiSearchDto> = await searchMulti(query, apiKey);
 
   switch (searchResult.status) {
     case ResponseStatus.ERROR:
       return [];
     case ResponseStatus.OK:
-      return mapAndFilterSearchResults(searchResult.data)
+      return mapAndFilterSearchResults(searchResult.data);
   }
-}
-
+};
 
 const mapAndFilterSearchResults = (response: TMDbMultiSearchDto): SearchItem[] => {
-  if (response.total_results === 0) return []
+  if (response.total_results === 0) return [];
 
   return response.results
     .filter((media): media is MovieResultDto | TVResultDto => isTVShow(media) || isMovie(media))
     .map((media) => {
-      if (isTVShow(media)) return mapTVShowToSearchItem(media)
-      else return mapMovieToSearchItem(media)
-    }).sort((a, b) => b.popularity - a.popularity)
-}
+      if (isTVShow(media)) return mapTVShowToSearchItem(media);
+      else return mapMovieToSearchItem(media);
+    })
+    .sort((a, b) => b.popularity - a.popularity);
+};
 
 const mapTVShowToSearchItem = (tvShow: TVResultDto): SearchItem => {
   return {
@@ -49,9 +49,9 @@ const mapTVShowToSearchItem = (tvShow: TVResultDto): SearchItem => {
     overview: tvShow.overview,
     posterUrl: getPosterUrl(tvShow.poster_path),
     releaseDate: tvShow.first_air_date,
-    originalTitle: tvShow.original_name
-  } as SearchItem
-}
+    originalTitle: tvShow.original_name,
+  } as SearchItem;
+};
 
 const mapMovieToSearchItem = (media: MovieResultDto): SearchItem => {
   return {
@@ -64,13 +64,11 @@ const mapMovieToSearchItem = (media: MovieResultDto): SearchItem => {
     overview: media.overview,
     posterUrl: getPosterUrl(media.poster_path),
     releaseDate: media.release_date,
-    originalTitle: media.original_title
-  } as SearchItem
-}
+    originalTitle: media.original_title,
+  } as SearchItem;
+};
 
 const getPosterUrl = (path: string | null): string => {
-  if (path !== null) return `${POSTER_BASE_URL}${path}`
+  if (path !== null) return `${POSTER_BASE_URL}${path}`;
   else return FALLBACK_POST_URL;
-}
-
-
+};
--- functions/src/tmdbGateway.ts
+++ functions/src/tmdbGateway.ts
@@ -1,22 +1,32 @@
-import axios, {AxiosResponse} from "axios";
-import {ApiResponse, ResponseError, ResponseStatus, ResponseSuccess, TMDbMultiSearchDto} from "./backendModels";
+import axios, { AxiosResponse } from 'axios';
+import {
+  ApiResponse,
+  ResponseError,
+  ResponseStatus,
+  ResponseSuccess,
+  TMDbMultiSearchDto,
+} from './backendModels';
 
-const baseUrl = 'https://api.themoviedb.org/3'
+const baseUrl = 'https://api.themoviedb.org/3';
 
-export const searchMulti = (query: string, apiKey: string): Promise<ApiResponse<TMDbMultiSearchDto>>  => {
-  const requestUrl = `${baseUrl}/search/multi?query=${query}&api_key=${apiKey}`
+export const searchMulti = (
+  query: string,
+  apiKey: string
+): Promise<ApiResponse<TMDbMultiSearchDto>> => {
+  const requestUrl = `${baseUrl}/search/multi?query=${query}&api_key=${apiKey}`;
 
-  return axios.get(requestUrl).then((response: AxiosResponse<TMDbMultiSearchDto>) => {
-    return Promise.resolve({
+  return axios
+    .get(requestUrl)
+    .then((response: AxiosResponse<TMDbMultiSearchDto>) => {
+      return Promise.resolve({
         status: ResponseStatus.OK,
-        data: response.data as TMDbMultiSearchDto
-      } as ResponseSuccess<TMDbMultiSearchDto>
-    )
-  }).catch((err) => {
-    return Promise.reject({
+        data: response.data as TMDbMultiSearchDto,
+      } as ResponseSuccess<TMDbMultiSearchDto>);
+    })
+    .catch((err) => {
+      return Promise.reject({
         status: ResponseStatus.ERROR,
-        data: err
-      } as ResponseError
-    )
-  })
-}
+        data: err,
+      } as ResponseError);
+    });
+};
