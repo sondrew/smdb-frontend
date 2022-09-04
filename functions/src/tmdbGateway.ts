b0cea4bb167a4429838127d8b996da3edb576368
Sondre <sondre.widmark@gmail.com>
2022-09-04 14:11:31 +0200

    Create tmdb api methods for retrieving details about a movie or tv show

--- functions/src/tmdbGateway.ts
+++ functions/src/tmdbGateway.ts
@@ -30,3 +30,62 @@ export const searchMulti = (
       } as ResponseError);
     });
 };
+export const getTVShowDetails = async (
+  tmdbId: number,
+  apiKey: string,
+  withProviders: boolean = false
+): Promise<ApiResponse<TVDetailsDto>> => {
+  const requestUrl = `${baseUrl}/tv/${tmdbId}?api_key=${apiKey}${
+    withProviders ? '&append_to_response=watch/providers' : ''
+  }`;
+  console.log('get tv');
+  return axios
+    .get(requestUrl)
+    .then((response: AxiosResponse<TVDetailsDto>) => {
+      console.log('tv then');
+      console.log(response.data.name);
+      return Promise.resolve({
+        status: ResponseStatus.OK,
+        data: response.data as TVDetailsDto,
+      } as ResponseSuccess<TVDetailsDto>);
+    })
+    .catch((err) => {
+      console.log('TV ERROR');
+      console.log(err);
+      return Promise.reject({
+        status: ResponseStatus.ERROR,
+        data: err,
+      } as ResponseError);
+    });
+};
+
+export const getMovieDetails = (
+  tmdbId: number,
+  apiKey: string,
+  withProviders: boolean = false
+): Promise<ApiResponse<MovieDetailsDto>> => {
+  const requestUrl = `${baseUrl}/movie/${tmdbId}?api_key=${apiKey}${
+    withProviders ? '&append_to_response=watch/providers' : ''
+  }`;
+
+  console.log('get movie');
+
+  return axios
+    .get(requestUrl)
+    .then((response: AxiosResponse<MovieDetailsDto>) => {
+      console.log('movie then');
+      console.log(response.data.title);
+      return Promise.resolve({
+        status: ResponseStatus.OK,
+        data: response.data as MovieDetailsDto,
+      } as ResponseSuccess<MovieDetailsDto>);
+    })
+    .catch((err) => {
+      console.log('MOVIE ERROR');
+      console.log(err);
+      return Promise.reject({
+        status: ResponseStatus.ERROR,
+        data: err,
+      } as ResponseError);
+    });
+};
