5bd9efdd59852135572b50be99eb3a50f547c16f
Sondre <sondre.widmark@gmail.com>
2022-09-05 20:17:17 +0200

    Use util method for verifying function request

--- functions/src/controller.ts
+++ functions/src/controller.ts
@@ -1,18 +1,23 @@
 import * as functions from 'firebase-functions';
 import { searchMovieOrTV } from './service';
 import { SearchItem } from '../../shared/models';
+import { CallableContext } from 'firebase-functions/lib/common/providers/https';
 
 exports.searchMoviesAndTV = functions
   .region('europe-west1')
   .runWith({ secrets: ['TMDB_API_KEY'] })
-  .https.onCall(async (data, context): Promise<SearchItem[]> => {
+  .https.onCall(async (query: string, context: CallableContext): Promise<SearchItem[]> => {
     console.log(context.app);
-    if (context.app == undefined) {
-      throw new functions.https.HttpsError(
-        'failed-precondition',
-        'The function must be called from an App Check verified app.'
-      );
-    }
+    returnErrorIfNotVerified(context);
 
-    return await searchMovieOrTV(data, process.env.TMDB_API_KEY ?? '');
+    return await searchMovieOrTV(query, process.env.TMDB_API_KEY ?? '');
   });
+
+const returnErrorIfNotVerified = (context: CallableContext) => {
+  if (context.app == undefined) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called from an App Check verified app.'
+    );
+  }
+};
