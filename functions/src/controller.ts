be1cbc1f0e0360d7031e88e5422d5cb386c46228
Sondre <sondre.widmark@gmail.com>
2022-09-11 16:35:17 +0200

    Log context if request is unverified

--- functions/src/controller.ts
+++ functions/src/controller.ts
@@ -8,7 +8,6 @@ exports.searchMoviesAndTV = functions
   .region('europe-west1')
   .runWith({ secrets: ['TMDB_API_KEY'] })
   .https.onCall(async (query: string, context: CallableContext): Promise<SearchItem[]> => {
-    console.log(context.app);
     returnErrorIfNotVerified(context);
 
     return await searchMovieOrTV(query, process.env.TMDB_API_KEY ?? '');
@@ -18,7 +17,6 @@ exports.createRecommendationList = functions
   .region('europe-west1')
   .runWith({ secrets: ['TMDB_API_KEY'] })
   .https.onCall(async (list: CreateListRequest, context: CallableContext) => {
-    console.log(context.app);
     returnErrorIfNotVerified(context);
 
     return await createRecommendationList(list, process.env.TMDB_API_KEY ?? '');
@@ -26,6 +24,8 @@ exports.createRecommendationList = functions
 
 const returnErrorIfNotVerified = (context: CallableContext) => {
   if (context.app == undefined) {
+    console.warn('Unverified request to a callable function received');
+    console.log(context);
     throw new functions.https.HttpsError(
       'failed-precondition',
       'The function must be called from an App Check verified app.'
