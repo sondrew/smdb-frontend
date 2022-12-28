11e830a3b95e95fec8f106028e65695eb80a7df7
Sondre <sondre.widmark@gmail.com>
2022-12-28 18:54:10 +0100

    Create slightly better view of movies and shows when viewing recommendation list

--- src/Components/Viewer/ViewRecommendationList.tsx
+++ src/Components/Viewer/ViewRecommendationList.tsx
@@ -3,63 +3,64 @@ import { useLocation, useParams } from 'react-router-dom';
 import { Box, Container, Heading, Text } from '@chakra-ui/react';
 import { RecommendationList } from '../../../shared/models';
 import { getRecommendationList } from '../../firestore';
+import ViewRecommendationListItem from './ViewRecommendationListItem';
 
 const ViewRecommendationList = () => {
   const { state } = useLocation(); // get recommendation list right after creation
-  let { listId } = useParams();
+  let { listId } = useParams(); // get list id from url path
+  console.log('listId', listId);
 
   const [recommendations, setRecommendations] = useState<RecommendationList | null>(
     state as RecommendationList
-  );
-
+  ); // TODO: Differentiatie between loading list and it not existing
   useEffect(() => {
+    console.log('USE EFFECT');
     if (recommendations == null && !!listId) {
-      // validate and sanitize id, so that it does not request on all random stuff that is not a valid id
+      // TODO: validate and sanitize id, so that it does not request on all random stuff that is not a valid id
       // alphanumeric 8 characters
       console.log('did not pass recommendation list prop, retrieving');
-
-      getRecommendationList(listId).then((res) => {
-        console.log({ res });
-        setRecommendations(res);
-      });
+      getRecommendationList(listId)
+        .then((res) => {
+          setRecommendations(res);
+        })
+        .catch(() => {
+          // TODO: Retrieving list failed, try again
+        });
     } else {
       console.log('sent recommendation list as prop, not retrieving');
       console.log({ recommendations });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log({ listId });
+  // remove result list when query is removed
+  // out of focus fjerne listen, in focus gå tilbake
 
   const renderRecommendations = recommendations?.list != null && recommendations.list.length > 0;
 
-  console.log({ state });
-
   return (
     <Container>
-      <Heading as="h1" size="md" pt={2} mb={10}>
-        Recommendation list
+      <Heading as="h1" size="md" pt={2} mb={8} color="grey">
+        Recommender3000
       </Heading>
       {recommendations === null && (
         <h2>There's no recommendation list here! Go to our front page</h2>
       )}
       {renderRecommendations && (
-        <>
-          <Heading as="h2" size="lg" mb={3} color="grey">
+        <Box>
+          <Heading as="h2" size="lg" mb={3} color="white">
             {recommendations.listName}
           </Heading>
           {!!recommendations?.listDescription && (
-            <Heading as="h3" size="sm" mt={2} color="red">
-              {recommendations.listDescription}
-            </Heading>
+            <Text as="i" mt={2} color="grey">
+              "{recommendations.listDescription}"
+            </Text>
           )}
-          {recommendations.list.map((item) => (
-            <Box key={item.id} m={1} border="1px solid yellow">
-              <Text>{item.title}</Text>
-              <Text color="grey">{item.description}</Text>
-              <Text fontStyle={'italic'}>"{item.userComment}"</Text>
-            </Box>
-          ))}
-        </>
+          <Box mt={8}>
+            {recommendations.list.map((item) => (
+              <ViewRecommendationListItem key={item.id} item={item} />
+            ))}
+          </Box>
+        </Box>
       )}
     </Container>
   );
---
+++ src/Components/Viewer/ViewRecommendationListItem.tsx
@@ -0,0 +1,52 @@
+import { RecommendedMedia } from '../../../shared/models';
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+const ViewRecommendationListItem = ({ item }: { item: RecommendedMedia }) => {
+  const [showDescription, setShowDescription] = useState<boolean>(false);
+  return (
+    <Box key={item.id} m={1} border="1px solid yellow">
+      <Flex flexDirection="row">
+        <Flex flexDirection="column" mx={3} alignItems="center" justifyContent="center">
+          <Text fontSize="4xl">{item.listIndex + 1}</Text>
+          <Image
+            objectFit={'fill'}
+            maxW={60}
+            maxH={100}
+            src={item.posterPath ?? ''}
+            alt={`Poster of ${item.title}`}
+            //{/* fallback */}
+          />
+        </Flex>
+        <Box padding={1}>
+          <Heading as="h3" fontSize="lg">
+            {item.title}
+          </Heading>
+          {item.userComment?.length !== 0 && (
+            <Text fontStyle={'italic'} my={1}>
+              "{item.userComment}"
+            </Text>
+          )}
+          {item.description?.length !== 0 && (
+            <>
+              <Text
+                color="lightgrey"
+                _hover={{
+                  color: 'white',
+                  cursor: 'pointer',
+                }}
+                onClick={() => setShowDescription((prev) => !prev)}
+              >
+                Description (+)
+              </Text>
+              {showDescription && <Text color="lightgray">{item.description}</Text>}
+            </>
+          )}
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+ViewRecommendationListItem.displayName = 'ViewRecommendationListItem';
+export default ViewRecommendationListItem;
