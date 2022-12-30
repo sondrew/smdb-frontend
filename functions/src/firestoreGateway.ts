ee0eca38762a174d4e4561b3934247a89360bd38
Sondre <sondre.widmark@gmail.com>
2022-12-30 18:30:14 +0100

    Create repo method in backend to get media items in list from list id

--- functions/src/firestoreGateway.ts
+++ functions/src/firestoreGateway.ts
@@ -1,11 +1,44 @@
 import { CreateRecommendationListEntity } from './entityModels';
+import { MediaIdsOfTypes } from './backendModels';
 
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+const db = admin.firestore();
 
 export const saveRecommendationList = async (
   list: CreateRecommendationListEntity
 ): Promise<string> => {
-  const addList = await admin.firestore().collection('recommendationLists').add(list);
+  const addList = await db.collection('recommendationLists').add(list);
   return addList.id;
+};
+
+export const getMediaItemsForList = async (listId: string): Promise<MediaIdsOfTypes> => {
+  const recommendationList = await db
+    .collection('recommendationLists')
+    .doc(listId)
+    .get()
+    .then((res: any) => {
+      return res.data();
+    })
+    .catch((err: any) => {
+      console.error('getMediaItemsForList: Failed retrieving list from db,', err);
+    });
+
+  // todo: rewrite with types
+  const movieIds = recommendationList.list
+    .filter((item: any) => item.mediaType === 'movie')
+    .map((item: any) => item.id);
+  const showIds = recommendationList.list
+    .filter((item: any) => item.mediaType === 'tv')
+    .map((item: any) => item.id);
+
+  const mediaIdAndMediaType: MediaIdsOfTypes = {
+    movies: movieIds,
+    shows: showIds,
+  };
+
+  return mediaIdAndMediaType;
 };
