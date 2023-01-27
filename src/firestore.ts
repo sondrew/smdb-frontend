44bd0c664bf0ab3d18059147002524a3c3495036
Sondre <sondre.widmark@gmail.com>
2023-01-27 20:05:42 +0100

    Include full URL for poster paths for media items

--- src/firestore.ts
+++ src/firestore.ts
@@ -13,21 +13,32 @@ export const getRecommendationList = async (listId: string): Promise<Recommendat
       id: docSnap.id,
       listName: data.listName,
       listDescription: data.listDescription,
-      list: data.list.map((media: RecommendedMedia) => ({
-        id: media.id,
-        listIndex: media.listIndex,
-        userRating: media.userRating,
-        userComment: media.userComment,
-        title: media.title,
-        originalTitle: media.originalTitle,
-        description: media.description,
-        mediaType: media.mediaType,
-        posterPath: media.posterPath,
-        imdbPath: media.imdbPath,
-        genres: media.genres,
-      })),
+      list: data.list
+        .map((media: RecommendedMedia) => ({
+          id: media.id,
+          listIndex: media.listIndex,
+          userRating: media.userRating,
+          userComment: media.userComment,
+          title: media.title,
+          originalTitle: media.originalTitle,
+          description: media.description,
+          mediaType: media.mediaType,
+          posterPath: getPosterUrl(media.posterPath),
+          imdbPath: media.imdbPath,
+          genres: media.genres,
+        }))
+        .sort((i: RecommendedMedia) => i.listIndex),
     } as RecommendationList;
   } else {
     return null;
   }
+};
+
+const FALLBACK_POST_URL =
+  'https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg';
+const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const getPosterUrl = (path: string | null): string => {
+  if (path !== null) return `${POSTER_BASE_URL}${path}`;
+  else return FALLBACK_POST_URL;
 };
