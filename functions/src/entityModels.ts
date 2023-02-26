724632aeaeaa0172d0efbb1ed61f635f7340f624
Sondre <sondre.widmark@gmail.com>
2023-02-26 18:52:39 +0100

    Create entity models for storing watch providers

--- functions/src/entityModels.ts
+++ functions/src/entityModels.ts
@@ -1,4 +1,5 @@
-import { MediaType, RecommendedMedia } from '../../shared/models';
+import { RecommendedMedia } from '../../shared/models';
+import { Timestamp } from 'firebase-admin/firestore';
 
 export type CreateRecommendationListEntity = {
   listName: string;
@@ -8,3 +9,23 @@ export type CreateRecommendationListEntity = {
 };
 
 export type RecommendedMediaEntity = RecommendedMedia;
+
+export type CreateMediaProvidersListEntity = {
+  id: string;
+  lastUpdated: Timestamp;
+  countries: { [countryCode: string]: CountryProvidersEntity } | null;
+};
+
+export type CountryProvidersEntity = {
+  link: string | null;
+  flatrate: StreamingOptionEntity[];
+  rent: StreamingOptionEntity[];
+  buy: StreamingOptionEntity[];
+};
+
+export type StreamingOptionEntity = {
+  displayPriority: number;
+  logoPath: string;
+  providerId: number;
+  providerName: string;
+};
